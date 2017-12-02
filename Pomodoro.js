d55d510760994b70b37ec4a79a64acd82deae824
Chris L <chris.luginbuhl@gmail.com>
2017-12-02 14:54:34 -0500

    lights, timer and hotkey from button press
    Green light = start working, red light = stop working 
    LIght goes from green to red after 20 min and bluetooth sends <CTRL>-D as a bluetooth keyboard to put monitor to sleep

---
+++ Pomodoro.js
@@ -0,0 +1,18 @@
+
+var kb = require("ble_hid_keyboard");
+NRF.setServices(undefined, { hid : kb.report });
+
+function btnPressed() {
+  digitalWrite([LED1,LED2,LED3], 0); //reset all LEDs  
+  digitalWrite(LED2,1);
+  var i = setTimeout(function()
+    {
+      kb.tap(kb.KEY.D, kb.MODIFY.CTRL);
+      digitalWrite(LED2,0);
+      digitalWrite(LED1,1);
+    }, 5000);
+}
+
+// trigger btnPressed whenever the button is pressed 
+setWatch(btnPressed, BTN, {edge:"rising",repeat:true,debounce:50});
+digitalWrite([LED1,LED2,LED3], 0);
