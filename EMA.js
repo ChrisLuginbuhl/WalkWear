7b7923cb6b66498bb1aaf29aadf66cda0835399c
Chris L <chris.luginbuhl@gmail.com>
2017-12-05 19:58:45 -0500

    Hold button to power off all LEDs

--- EMA.js
+++ EMA.js
@@ -1,17 +1,29 @@
 var kb = require("ble_hid_keyboard");
 NRF.setServices(undefined, { hid : kb.report });
+//var timeout_inactivity;
+var tomatoCounter = 0;
 
-function btnPressed() {
-  digitalWrite([LED1,LED2,LED3], 0); //reset all LEDs
-  digitalWrite(LED2,1);
-  var i = setTimeout(function()
-    {
-      kb.tap(kb.KEY.D, kb.MODIFY.CTRL);
-      digitalWrite([LED1,LED2,LED3], 0); //turn off all LEDs
-      digitalWrite(LED1,1);
-    }, 5000);
+setWatch(function(btnPressed){
+    var isLong = (btnPressed.time - btnPressed.lastTime) > 0.4;
+    print(isLong.toString());
+    if (isLong) lightsOff();
+    else startWork();
+  }, BTN, {repeat:true, debounce:50, edge:"falling"});
+
+function lightsOff(){
+  console.log("Turn off everything");
+  digitalWrite([LED1,LED2,LED3], 0);
+  //setTimeout(function () { digitalWrite(D23,0); }, 1000);
 }
 
-// trigger btnPressed whenever the button is pressed
-setWatch(btnPressed, BTN, {edge:"rising",repeat:true,debounce:50});
-digitalWrite([LED1,LED2,LED3], 0);
+function startWork(){
+  console.log("Start work");
+  tomatoCounter += 1;
+  digitalWrite(LED1, 0);
+  digitalWrite(LED2, 1); //Green LED
+  setTimeout(function () {
+    digitalWrite(LED1, 1); //Red LED
+    digitalWrite(LED2, 0);
+    kb.tap(kb.KEY.D, kb.MODIFY.CTRL);
+  }, 5000);
+}
