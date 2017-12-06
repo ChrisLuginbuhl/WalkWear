2718d52f3f4fccee8d6b68150bd68d5ae94dddbb
Chris L <chris.luginbuhl@gmail.com>
2017-12-06 12:17:37 -0500

    Green blinking light warns when break time is over.

--- EMA.js
+++ EMA.js
@@ -1,7 +1,19 @@
 var kb = require("ble_hid_keyboard");
 NRF.setServices(undefined, { hid : kb.report });
-//var timeout_inactivity;
+
+const WORK_DURATION = 5000; //20mins
+const REST_DURATION = 5000;
+const START_WORK_ALERT_DURATION = 8000; //1 min
+
+//const WORK_DURATION = 60*1000*20; //20mins
+//const REST_DURATION = 60 * 1000 * 5; //5min
+//const START_WORK_ALERT_DURATION = 60 * 1000 * 1; //1 min
+
+
 var tomatoCounter = 0;
+var a = 1;
+var blink;
+var work;
 
 setWatch(function(btnPressed){
     var isLong = (btnPressed.time - btnPressed.lastTime) > 0.4;
@@ -13,17 +25,37 @@ setWatch(function(btnPressed){
 function lightsOff(){
   console.log("Turn off everything");
   digitalWrite([LED1,LED2,LED3], 0);
-  //setTimeout(function () { digitalWrite(D23,0); }, 1000);
+  clearInterval(work);
+  blink = undefined;
+}
+
+function startBreak() {
+    console.log("Start break");
+    digitalWrite(LED1, 1); //Red LED
+    digitalWrite(LED2, 0);
+    kb.tap(kb.KEY.D, kb.MODIFY.CTRL);
+    setTimeout(startWorkAlert, REST_DURATION);
+}
+
+function startWorkAlert() {
+  console.log("Start work alert");
+  LED1.write(0);
+  blink = setInterval("digitalWrite(LED2, a=!a)", 333);
+  var stopBlink = setTimeout(function () {
+    clearInterval(blink);
+    blink = undefined;
+    digitalWrite([LED1,LED2,LED3], 0);  
+    console.log("Start work alert timed out - shutting off");
+  }, START_WORK_ALERT_DURATION);
+  clearTimeout(stopBlink);
 }
 
 function startWork(){
   console.log("Start work");
   tomatoCounter += 1;
+  console.log(blink);
+  if (blink !== undefined) clearInterval(blink);
   digitalWrite(LED1, 0);
   digitalWrite(LED2, 1); //Green LED
-  setTimeout(function () {
-    digitalWrite(LED1, 1); //Red LED
-    digitalWrite(LED2, 0);
-    kb.tap(kb.KEY.D, kb.MODIFY.CTRL);
-  }, 5000);
+  rest = setTimeout(startBreak, WORK_DURATION); 
 }
